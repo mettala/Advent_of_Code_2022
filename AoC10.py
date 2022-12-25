55612b7bc52ba5bfdd09f0e03418bb41debd5651
mettala <47830974+mettala@users.noreply.github.com>
2022-12-25 23:18:04 +0200

    Add files via upload
    comments added

--- AoC10.py
+++ AoC10.py
@@ -1,14 +1,17 @@
 def check_cycle(cycle, value, x, sum):
+    #calculate signal strength, if 20th cycle or (cycle-20)/40 reminder is 0
     x = x + value
     if  cycle == 20 or (cycle > 20 and (cycle-20) % 40 == 0):
         sum = sum + x * cycle
     return x, sum
 
 def add_pixel_row(pixels):
+    #add new pixel row to CRT
     pixels.append('')
     return pixels
 
 def add_pixel(pixels,cycle,x):
+    #add pixel value to current location
     if x - 1 <= cycle and cycle <= x + 1:
         pixel = '#'
     else:
@@ -17,6 +20,7 @@ def add_pixel(pixels,cycle,x):
     return pixels
 
 def add_cycle(cycle, pixels):
+    #move to next pixel and add new row if needed
     cycle = cycle + 1
     if cycle % 41 == 0:
         pixels = add_pixel_row(pixels)
@@ -30,6 +34,7 @@ x = 1
 sum = 0
 
 for r in input:
+    # part A
     cycle = cycle + 1
     if r == 'noop':
         x, sum = check_cycle(cycle, 0, x, sum)
@@ -46,6 +51,7 @@ x = 1
 cycle = 1
 
 for r in input:
+    # part B
     pixels = add_pixel(pixels,cycle,x)
     cycle, pixels = add_cycle(cycle,pixels)
 
